662199df820a616dada299f095d4162a3bf86c95
Serhii Kochniev <sergeykochnev4149@gmail.com>
2023-01-18 22:12:36 +0200

    Update UI
    Add widget with info about playing song
    and buttons for back/play/next. Also add progress bar
    for showing time of listening

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,24 +2,30 @@ package com.example.musicplayer;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.musicplayer.fragments.PlayingSong;
+import com.example.musicplayer.fragments.SavedMusic;
+import com.example.musicplayer.fragments.YoutubeMusic;
+
 
 @SuppressLint("ResourceAsColor")
 
 public class MainActivity extends AppCompatActivity {
     private boolean isActive_SavedMusic;
     ImageView ivYoutubeMusic, ivSavedMusic;
-    TextView tvMusic, tvFiles;
+    TextView tvMusic, tvFiles, tvSoundtrackName;
     CardView cvMusic, cvFiles;
-    int colour_activeFragment, colour_inactiveFragment;
+    int colour_activeFragment, colour_inactiveFragment, flFragmentID = R.id.flFragment;
+    FragmentTransaction fragmentTransaction;
+    YoutubeMusic youtubeMusicFragment;
+    PlayingSong playingSongFragment;
+    SavedMusic savedMusicFragment;
 
 
     @Override
@@ -36,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         tvMusic = findViewById(R.id.tvMusic);
         ivSavedMusic = findViewById(R.id.ivSavedMusic);
         tvFiles = findViewById(R.id.tvFiles);
+        findViewById(R.id.tvSoundtrackName).setSelected(true);
     }
-
 
 
     @Override
@@ -59,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         tvMusic.setTextColor(colour_activeFragment);
         ivSavedMusic.setColorFilter(colour_inactiveFragment);
         tvFiles.setTextColor(colour_inactiveFragment);
+        setFragment();
     }
 
     public void goToSavedMusic() {
@@ -68,11 +75,25 @@ public class MainActivity extends AppCompatActivity {
         tvMusic.setTextColor(colour_inactiveFragment);
         ivSavedMusic.setColorFilter(colour_activeFragment);
         tvFiles.setTextColor(colour_activeFragment);
+        setFragment();
     }
 
 
-    private void setOnClickMethods(){
+    private void setOnClickMethods() {
         cvMusic.setOnClickListener(v -> goToYoutubeMusic());
         cvFiles.setOnClickListener(v -> goToSavedMusic());
+    }
+
+    private void setFragment() {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if (isActive_SavedMusic) {
+            savedMusicFragment = new SavedMusic();
+            fragmentTransaction.replace(flFragmentID, savedMusicFragment);
+        } else {
+            youtubeMusicFragment = new YoutubeMusic();
+            fragmentTransaction.replace(flFragmentID, youtubeMusicFragment);
+        }
+        fragmentTransaction.commit();
+
     }
 }
